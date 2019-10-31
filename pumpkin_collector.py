f705f88f141241fb871d2a73bd139b00fa30e297
ethanalvares <47458407+ethanalvares@users.noreply.github.com>
2019-10-30 23:29:13 -0400

    Update pumpkin_collector.py

--- pumpkin_collector.py
+++ pumpkin_collector.py
@@ -1,3 +1,4 @@
+
 import random
 import arcade
 
@@ -5,17 +6,80 @@ PLAYER_SCALING = 0.5
 PUMPKIN_SCALING = 0.2
 PUMPKIN_COUNT = 50
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Spooky Collection"
+WIDTH = 800
+HEIGHT = 600
+TITLE = "Spooky Collection"
+
+current_screen = "menu"
 
 
-class MyGame(arcade.Window):
+def on_draw():
+    arcade.start_render()
+    # Draw in here...
+    if current_screen == "menu":
+        draw_menu()
+    elif current_screen == "instructions":
+        draw_instructions()
+    elif current_screen == "play":
+        draw_play()
 
+
+def on_key_press(key, modifiers):
+    global current_screen
+
+    if current_screen == "menu":
+        menu_keybinds(key, modifiers)
+    elif current_screen == "instructions":
+        instructions_keybinds(key, modifiers)
+    elif current_screen == "play":
+        play_keybinds(key, modifiers)
+
+
+def menu_keybinds(key, modifiers):
+    global current_screen
+    if key == arcade.key.I:
+        current_screen = "instructions"
+    elif key == arcade.key.P:
+        current_screen = "play"
+    elif key == arcade.key.ESCAPE:
+        exit()
+
+
+def instructions_keybinds(key, modifiers):
+    global current_screen
+    if key == arcade.key.ESCAPE:
+        current_screen = "menu"
+
+
+def play_keybinds(key, modifiers):
+    global current_screen
+    if key == arcade.key.ESCAPE:
+        current_screen = "menu"
+
+def draw_menu():
+    arcade.set_background_color(arcade.color.BLACK)
+    arcade.draw_text("Main Menu", WIDTH/2, HEIGHT/2,
+                     arcade.color.SKY_BLUE, font_size=30, anchor_x="center")
+    arcade.draw_text("I for Instructions", WIDTH/2, HEIGHT/2-60,
+                     arcade.color.SKY_BLUE, font_size=20, anchor_x="center")
+    arcade.draw_text("P to Play", WIDTH/2, HEIGHT/2-90,
+                     arcade.color.SKY_BLUE, font_size=20, anchor_x="center")
+
+
+def draw_instructions():
+    arcade.set_background_color(arcade.color.BLACK)
+    arcade.draw_text("Instructions", WIDTH/2, HEIGHT/2,
+                     arcade.color.SKY_BLUE, font_size=30, anchor_x="center")
+    arcade.draw_text("Use the mouse to move", WIDTH/2, HEIGHT/2-60,
+                     arcade.color.SKY_BLUE, font_size=20, anchor_x="center")
+
+
+
+def draw_play():
     def __init__(self):
 
 
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__(WIDTH, HEIGHT, TITLE)
 
         self.set_mouse_visible(False)
 
@@ -46,8 +110,8 @@ class MyGame(arcade.Window):
 
             pumpkin = arcade.Sprite("images/pumpkin", PUMPKIN_SCALING)
 
-            pumpkin.center_x = random.randrange(SCREEN_WIDTH)
-            pumpkin.center_y = random.randrange(SCREEN_HEIGHT)
+            pumpkin.center_x = random.randrange(WIDTH)
+            pumpkin.center_y = random.randrange(HEIGHT)
 
             self.pumpkin_list.append(pumpkin)
 
@@ -63,7 +127,7 @@ class MyGame(arcade.Window):
         self.player_sprite.center_x = x
         self.player_sprite.center_y = y
 
-    
+
 
     def on_update(self, delta_time):
         global draw_scare()
@@ -80,16 +144,21 @@ class MyGame(arcade.Window):
 
     def draw_scare(self):
         arcade.start_render()
-        arcade.draw_texture_rectangle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT, 'images/skeleton')
+        arcade.draw_texture_rectangle(WIDTH/2, HEIGHT/2, WIDTH, HEIGHT, 'images/skeleton')
 
 
 
 
 def setup():
-    window = MyGame()
-    window.setup()
+    arcade.open_window(WIDTH, HEIGHT, "BLACK")
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.schedule(update, 1 / 60)
+
+    # Override arcade window methods
+    window = arcade.get_window()
+    window.on_draw = on_draw
+    window.on_key_press = on_key_press
     arcade.run()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     setup()
